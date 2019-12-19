f1aac946ed48c8acd8dbea239b4cd4b4b8118dad
bhoutik kaneriya <bbkaneriya@webroot.com>
2019-12-19 15:02:34 -0800

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fileIndexPath = path.join(__dirname + '/app.js')
 
 //simpleGit().add([fileIndexPath], (err, data) => console.log(err))
 
-simpleGit().init().add(fileIndexPath).commit('first commit').addRemote('origin', 'https://github.com/bhoutikkaneriya/test-git-node.git').push('origin','master')
+simpleGit().init().add(fileIndexPath).commit('first commit').addRemote('origin', 'https://github.com/bhoutikkaneriya/test-git-node.git').push(['-u','origin','master'])
 
 
 
